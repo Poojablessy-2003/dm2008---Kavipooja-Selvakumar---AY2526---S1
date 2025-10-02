b440487b916c7b7bd4c89f579f6919f36d11b32e
Poojablessy-2003 <kavipoojantu@gmail.com>
2025-10-02 20:45:36 +0800

    commit
    commit

---
+++ 01-activities/Activity 1A/sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background(204,204,255);
+  
+  // YOUR CODE HERE
+  fill(204,102,0)
+  ellipse(200,200,200,200)
+  fill(0,0,0)
+  ellipse(165,165,25,25)
+  ellipse(237,165,25,25)
+  line(185,199,226,199)
+  line(185,199,201,220)
+  line(201,220,226,199)
+  line(76,177,120,211)
+  line(120,211, 75,224)
+  line(269,207, 323,176)
+  line(269,207, 323,225)
+  line(120,138, 120,85)
+  line(120,85,158,110)
+  line(249,110,290,87)
+  line(290,87, 280,136)
+  fill(204,102,0)
+  ellipse(202,372,145,145)
+  ellipse(298,398,50,50)
+  // YOUR CODE HERE
+  
+  
+}
